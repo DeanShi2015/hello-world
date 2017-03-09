2507789b302243073f386ec2b8859096a6b4da05
DeanShi2015 <970660665@qq.com>
2017-03-09 18:05:25 +0800

    Test first time

--- src/com/celnet/demo/helloWorld.java
+++ src/com/celnet/demo/helloWorld.java
@@ -6,5 +6,6 @@ package com.celnet.demo;
 public class helloWorld {
     public static void main (String[] args){
         System.out.println("Hello World");
+        System.out.println("TestGit");
     }
 }
